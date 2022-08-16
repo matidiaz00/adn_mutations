497dce1ae687889dfc2557bd57b0ceac670c3531
Matias Diaz <mdiaz@alquilando.com>
2022-08-16 02:02:31 -0300

    Test for problems when deploy to server

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,20 @@
 import { https } from 'firebase-functions'
 import MainRoute from './routes/main.route'
 import express from 'express'
-//import { HeadersMiddleWare, ParserJSONMiddleWare, ParserURLMiddleWare, StaticMiddleWare } from './middlewares/main.middleware'
-//import v1Route from './routes/v1/v1.route'
+import { HeadersMiddleWare, ParserJSONMiddleWare, ParserURLMiddleWare, StaticMiddleWare } from './middlewares/main.middleware'
+import v1Route from './routes/v1/v1.route'
 
 /** Express object */
 const app = express();
 
 /** Setting General Middlewares */
-//app.use(HeadersMiddleWare, ParserJSONMiddleWare, ParserURLMiddleWare, StaticMiddleWare);
+app.use(HeadersMiddleWare, ParserJSONMiddleWare, ParserURLMiddleWare, StaticMiddleWare);
 
 /** Main Routes */
 app.use('/api', MainRoute);
 
 /** ADN Routes */
-//app.use('/api/v1', v1Route);
+app.use('/api/v1', v1Route);
 
 /** Export functions for Firebase */
 exports.app = https.onRequest(app)
